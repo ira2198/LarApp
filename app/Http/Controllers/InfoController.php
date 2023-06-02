8a2e5dcf0ab1644776abbd64bdd659e515dd015f
ira2198 <83193516+ira2198@users.noreply.github.com>
2023-06-02 19:10:52 +0300

    Homework 1

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class InfoController extends Controller
+{
+    public function __invoke()
+    {
+        return "About as";
+    }    
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InfoController;
-use App\Http\Controllers\WelcomController;
+use App\Http\Controllers\WelcomeController; 
 use App\Http\Controllers\NewsController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
-});
+ return view('welcome');
+}); 
+ 
+
+Route::get('info', InfoController::class);
+
+Route::get('hello/{name}', [WelcomeController::class, 'show']);
+
+Route::get('news', NewsController::class);
